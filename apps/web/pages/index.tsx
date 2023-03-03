bb1177d1f6be61d80b5bf23ee54c35af38a08675
gaopengyue <gaopengyue@sinochem.com>
2023-03-03 20:16:57 +0800

    feat: create project

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,39 @@
-import { Button } from "ui";
+import React, { useRef, useState } from 'react'
+import { Button, Container } from '@nextui-org/react';
+import * as THREE from 'three'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import 'ui/global.css';
+
+
+function Box(props: ThreeElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <Container css={{ height: '100vh' }}>
+      <Button>主界面</Button>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </Container>
   );
 }
